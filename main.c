7b6b7209455364f8b8aab32b84b4df4c1e72fb70
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2022-09-12 09:09:01 +0100

    Create main.c

--- main.c
+++ main.c
@@ -1,97 +1,53 @@
 #include "monty.h"
 
+char *flag = "stack";
+
 /**
- * main - main function
- * @argc: argument counts
- * @argv: arguments passed
- * Return: success
+ * main - main function to run monty
+ * @ac: number of arguments
+ * @av: list of arguments as strings
+ *
+ * Return: 0
  */
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-	FILE *file;
-	char *buff = NULL, *string;
-	size_t s = 0;
-	unsigned int linenum = 1;
-	stack_t *stk = NULL;
+	stack_t *h;
+	int exec_err, fp;
+	unsigned int line_number;
+	ssize_t status;
+	char *line;
+	size_t length;
 
-	variables.check = 0;
-
-	if (argc != 2)
+	if (ac != 2)
 	{
-		printf("USAGE: monty file\n");
+		printf("USAGE: monty file\n"), exit(EXIT_FAILURE);
+	}
+	h = NULL;
+	fp = open(av[1], O_RDONLY);
+	if (fp == -1)
+	{
+		printf("Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(argv[1], "r");
-	if (file == NULL)
-	{
-		printf("Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (getline(&buff, &s, file) != -1)
-	{
-		if (*buff != '\n')
+	line_number = 0;
+	do {
+		++line_number;
+		line = NULL;
+		length = 0;
+		status = _getline(&line, &length, fp);
+		if (status > 2)
 		{
-			string = strtok(buff, "\n");
-			_tokenizer(string, &stk, linenum);
+			exec_err = execute(&h, line, line_number);
+			if (exec_err == -1)
+				status = -2;
 		}
-		linenum++;
-	}
-	fclose(file);
-	free(buff);
-	if (stk != NULL)
-		free_stk(&stk, linenum);
-	return (EXIT_SUCCESS);
-}
+		else
+			free(line);
+	} while (status >= 0);
 
-/**
- * _tokenizer - function to tokenize strings and commands
- * @string: string to be tokenized
- * @stk: pointer to the stack
- * @linenum: line numbers
- * Return: void
- */
-void _tokenizer(char *string, stack_t **stk, unsigned int linenum)
-{
-	char *token;
-	char *tokens;
-
-	token = strtok(string, " ");
-	if (token == NULL || *token == ' ' || *token == '\n' || *token == '#')
-		return;
-	if (strcmp(token, "push") == 0)
-	{
-		tokens = token;
-		token = strtok(NULL, " ");
-		if (!check_digit(token))
-		{
-			printf("L%d: usage: push integer\n", linenum);
-			free_stk(stk, linenum);
-			exit(EXIT_FAILURE);
-		}
-		variables.holder = atoi(token);
-		_ops(tokens, stk, linenum);
-	}
-	else
-		_ops(token, stk, linenum);
-}
-
-/**
- * check_digit - checks if string is a number
- * @token: string to check
- * Return: 1 if number, 0 if not
- */
-int check_digit(char *token)
-{
-	if (token == NULL)
+	close(fp);
+	free_stack(h), h = NULL;
+	if (status == -1)
 		return (0);
-	if (*token == '-')
-		token++;
-	while (*token != '\0')
-	{
-		if (!isdigit(*token))
-			return (0);
-		token++;
-	}
-	token++;
-	return (1);
+	exit(EXIT_FAILURE);
 }
